289477c5cd3199d3c546a4255095f3e36a31f7f6
imrulkayes75 <ikbubt@gmail.com>
2024-04-09 22:09:07 +0600

    MonDb Create,Read & Delete operation added on server site

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // midlewire
 app.use(cors());
@@ -29,12 +29,33 @@ async function run() {
         await client.connect();
         const coffeeCollection = client.db('coffeeDb').collection('coffee')
 
+        app.get('/coffee', async (req, res) => {
+            const cursor = coffeeCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        app.get('/coffee/:id', async (req, res) => {
+            const id = req.params.id;
+            const quary = { _id: new ObjectId(id) }
+            const result = await coffeeCollection.findOne(quary);
+            res.send(result);
+        })
+
         app.post('/coffee', async (req, res) => {
             const newCoffee = req.body;
             console.log(newCoffee);
             const result = await coffeeCollection.insertOne(newCoffee)
             res.send(result)
         })
+
+        app.delete('/coffee/:id', async (req, res) => {
+            const id = req.params.id;
+            const quary = { _id: new ObjectId(id) }
+            const result = await coffeeCollection.deleteOne(quary);
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
